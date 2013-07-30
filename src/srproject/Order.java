5e79b6b45207edb5f6a2807bc3326fc67f775e20
Paul Durivage <pauldurivage@gmail.com>
2013-07-30 14:58:08 -0500

    Object instance name changes

--- src/srproject/Order.java
+++ src/srproject/Order.java
@@ -1,17 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package srproject;
 
-/**
- *
- * @author Bradley
- */
 public class Order {
     Customer customer;
-    Employee salesPerson;
-    CatalogItem media;
+    Employee employee;
+    CatalogItem item;
     
     
     
