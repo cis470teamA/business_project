92b7b741caa080a436208af22e1acf39a866e1e3
Paul Durivage <paul.durivage@rackspace.com>
2013-08-06 19:14:37 -0500

    Initial commit of MysqlConn class

--- src/wsc_application/MysqlConn.java
+++ src/wsc_application/MysqlConn.java
@@ -1,5 +1,32 @@
 package wsc_application;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class MysqlConn {
-    // Code
+    private String user = "appl_user";
+    private String pass = "4Eche4AfETupHu";
+    private Connection conn;
+    
+    public MysqlConn () {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        }
+        catch (Exception ex) {
+            System.out.println("Exception loading com.mysql.jdbc.Driver" + ex.getMessage());
+        }
+    }
+    
+    public void makeConnection() {
+        try {
+            this.conn = DriverManager.getConnection("jdbc:mysql://mysql.durivage.org", user, pass);
+        }
+        catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
 }
