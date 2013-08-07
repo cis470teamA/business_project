51e3a7f2b529188b2497078c2ad1204e64010009
Paul Durivage <paul.durivage@rackspace.com>
2013-08-07 14:17:08 -0500

    Add messaging.

--- src/wsc_application/Messaging.java
+++ src/wsc_application/Messaging.java
@@ -1,6 +1,24 @@
-
 package wsc_application;
 
+import javax.swing.JOptionPane;
+
 public class Messaging {
-    
+    public static void sendMessage(String recipient) {
+        JOptionPane.showMessageDialog(null, 
+                "Sending message to " + recipient + ".",
+                "Sending Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+    public static void sendMessage(String recipient, String message) {
+        JOptionPane.showMessageDialog(null, 
+                "To: " + recipient + ".\n" + 
+                "Body: " + message,
+                "Sending Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+    public static void sendMessage(String recipient, String message, String subject) {
+        JOptionPane.showMessageDialog(null, 
+                "To: " + recipient + ".\n" +
+                "Subject: " + subject +
+                "Body: " + message,
+                "Sending Message", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
