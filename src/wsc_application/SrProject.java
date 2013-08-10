5891a120fbe907ea74a87f69134907c2e56ec662
Paul Durivage <paul.durivage@rackspace.com>
2013-08-10 14:45:28 -0500

    More test statements in the main class

--- src/wsc_application/SrProject.java
+++ src/wsc_application/SrProject.java
@@ -1,8 +1,10 @@
 
 package wsc_application;
 
+import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+
 
 public class SrProject {
 
@@ -12,7 +14,7 @@ public class SrProject {
     public static void main(String[] args) {
         createAndShowGUI();
         MysqlConn mysql = new MysqlConn();
-//        Paul's testing functions
+//        Paul's testing statements
 //        Customer customer = Customer.searchBy("CUSTID", "1");
 //        customer.setFName("Billy");
 //        customer = Customer.updateBy(customer);
@@ -22,6 +24,16 @@ public class SrProject {
 //        if (Customer.isCustomer(1)) {
 //            JOptionPane.showMessageDialog(null, "Is customer", null, JOptionPane.INFORMATION_MESSAGE);
 //        }
+//        ArrayList<Order> testlist = Order.getOrders(1);
+//        for (Order o : testlist) {
+//            System.out.println("OrderId:" + o.getORDID());
+//        }
+//        Order order = Order.getOrder(1);
+//        System.out.println("Customer " + order.getCustomer().getCustFName());
+//        order.setContent("We all love kittens, yes we do, kittens are fun and stuff.");
+//        order.setMediaStatus("Ordered");
+//        order.setMediaStatus("T-Shirt");
+//        order = Order.createOrder(order);
     }
 
     private static void createAndShowGUI() {
